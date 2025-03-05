d08d9a16cb8e19d412d5bfe93aada3c65b83c89c
Dario <darkrupnova@gmail.com>
2025-03-05 12:38:59 +0300

    Lab_2 Final commit

--- Android/Lab_1/app/src/main/java/com/example/lab_1/MainActivity.java
+++ Android/Lab_1/app/src/main/java/com/example/lab_1/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.lab_1;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.lab_1.databinding.ActivityMainBinding;
 
@@ -33,8 +43,23 @@ public class MainActivity extends AppCompatActivity {
         crypt = decrypt(key, crypt);
 
         // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+        //TextView tv = binding.sampleText;
+        //tv.setText(stringFromJNI());
+        activityResultLauncher  = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback() {
+                    @Override
+                    public void onActivityResult(Object o) {
+                        Class<ActivityResult> c = ActivityResult.class;
+                        ActivityResult result = c.cast(o);
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            // обработка результата
+                            String pin = data.getStringExtra("pin");
+                            Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     /**
@@ -47,4 +72,33 @@ public class MainActivity extends AppCompatActivity {
     public static  native byte[] encrypt(byte[] key, byte[] data);
 
     public static  native byte[] decrypt(byte[] key, byte[] data);
+    public static byte[] stringToHex(String s)
+    {
+        byte[] hex;
+        try
+        {
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex)
+        {
+            hex = null;
+        }
+        return hex;
+    }
+    /*public void onButtonClick(View v)
+    {
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+    }*/
+    /*public void onButtonClick(View v)
+    {
+        Intent it = new Intent(this, PinpadActivity.class);
+        startActivity(it);
+    }*/
+    public void onButtonClick(View v)
+    {
+        Intent it = new Intent(this, PinpadActivity.class);
+        //startActivity(it);
+        activityResultLauncher.launch(it);
+    }
+    ActivityResultLauncher activityResultLauncher;
 }
