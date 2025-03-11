2eb0a10854f27558820a9120b0e02a33d3761f6a
Dario <darkrupnova@gmail.com>
2025-03-11 12:21:38 +0300

    Lab_3 Final commit

--- Android/Lab_1/app/src/main/java/com/example/lab_1/MainActivity.java
+++ Android/Lab_1/app/src/main/java/com/example/lab_1/MainActivity.java
@@ -17,8 +17,10 @@ import android.widget.Toast;
 
 import com.example.lab_1.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity implements TransactionEvents {
+
+    public native boolean transaction(byte[] trd);
     // Used to load the 'lab_1' library on application startup.
     static {
         System.loadLibrary("lab_1");
@@ -45,7 +47,8 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         //TextView tv = binding.sampleText;
         //tv.setText(stringFromJNI());
-        activityResultLauncher  = registerForActivityResult(
+        stringFromJNI();
+        activityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback() {
                     @Override
@@ -55,36 +58,40 @@ public class MainActivity extends AppCompatActivity {
                         if (result.getResultCode() == Activity.RESULT_OK) {
                             Intent data = result.getData();
                             // обработка результата
-                            String pin = data.getStringExtra("pin");
-                            Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                            //String pin = data.getStringExtra("pin");
+                            //Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                            pin = data.getStringExtra("pin");
+                            synchronized (MainActivity.this) {
+                                MainActivity.this.notifyAll();
+                            }
                         }
                     }
                 });
     }
-
     /**
      * A native method that is implemented by the 'lab_1' native library,
      * which is packaged with this application.
      */
     public native String stringFromJNI();
-    public static native int initRng();
-    public static native byte[] randomBytes(int no);
-    public static  native byte[] encrypt(byte[] key, byte[] data);
 
-    public static  native byte[] decrypt(byte[] key, byte[] data);
-    public static byte[] stringToHex(String s)
-    {
+    public static native int initRng();
+
+    public static native byte[] randomBytes(int no);
+
+    public static native byte[] encrypt(byte[] key, byte[] data);
+
+    public static native byte[] decrypt(byte[] key, byte[] data);
+
+    public static byte[] stringToHex(String s) {
         byte[] hex;
-        try
-        {
+        try {
             hex = Hex.decodeHex(s.toCharArray());
-        }
-        catch (DecoderException ex)
-        {
+        } catch (DecoderException ex) {
             hex = null;
         }
         return hex;
     }
+
     /*public void onButtonClick(View v)
     {
         Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
@@ -94,11 +101,54 @@ public class MainActivity extends AppCompatActivity {
         Intent it = new Intent(this, PinpadActivity.class);
         startActivity(it);
     }*/
-    public void onButtonClick(View v)
-    {
-        Intent it = new Intent(this, PinpadActivity.class);
+    public void onButtonClick(View v) {
+        //Intent it = new Intent(this, PinpadActivity.class);
         //startActivity(it);
-        activityResultLauncher.launch(it);
+        //activityResultLauncher.launch(it);
+       /* new Thread(()-> {
+            try {
+                byte[] trd = stringToHex("9F0206000000000100");
+                boolean ok = transaction(trd);
+                runOnUiThread(()-> {
+                    Toast.makeText(MainActivity.this, ok ? "ok" : "failed", Toast.LENGTH_SHORT).show();
+                });
+
+            } catch (Exception ex) {
+                // todo: log error
+            }
+        }).start();*/
+        byte[] trd = stringToHex("9F0206000000000100");
+        transaction(trd);
     }
+
     ActivityResultLauncher activityResultLauncher;
+
+    private String pin;
+
+    @Override
+    public String enterPin(int ptc, String amount) {
+        pin = new String();
+        Intent it = new Intent(MainActivity.this, PinpadActivity.class);
+        it.putExtra("ptc", ptc);
+        it.putExtra("amount", amount);
+        synchronized (MainActivity.this) {
+            activityResultLauncher.launch(it);
+            try {
+                MainActivity.this.wait();
+            } catch (Exception ex) {
+                //todo: log error
+            }
+        }
+        return pin;
+
+
+    }
+    @Override
+    public void transactionResult(boolean result) {
+        runOnUiThread(()-> {
+            Toast.makeText(MainActivity.this, result ? "ok" : "failed", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+
 }
--- Android/Lab_1/app/src/main/java/com/example/lab_1/PinpadActivity.java
+++ Android/Lab_1/app/src/main/java/com/example/lab_1/PinpadActivity.java
@@ -12,6 +12,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
+
 public class PinpadActivity extends AppCompatActivity {
 
     //@Override
@@ -35,6 +37,19 @@ public class PinpadActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pinpad);
 
         tvPin = findViewById(R.id.txtPin);
+        TextView ta = findViewById(R.id.txtAmount);
+        String amt = String.valueOf(getIntent().getStringExtra("amount"));
+        Long f = Long.valueOf(amt);
+        DecimalFormat df = new DecimalFormat("#,###,###,##0.00");
+        String s = df.format(f);
+        ta.setText("Сумма: " + s);
+
+        TextView tp = findViewById(R.id.txtPtc);
+        int pts = getIntent().getIntExtra("ptc", 0);
+        if (pts == 2)
+            tp.setText("Осталось две попытки");
+        else if (pts == 1)
+            tp.setText("Осталась одна попытка");
 
         ShuffleKeys();
 
@@ -52,6 +67,7 @@ public class PinpadActivity extends AppCompatActivity {
             pin = "";
             tvPin.setText("");
         });
+
     }
     public void keyClick(View v)
     {
