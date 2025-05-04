33f93e36d9b3bd7e40b9d72b95b7c4f42db57def
Darkrui <darkrupnova@gmail.com>
2025-05-04 22:43:44 +0300

    troubleshooting lab_9

--- spring/backend/front/src/components/NavigationBar.jsx
+++ spring/backend/front/src/components/NavigationBar.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faUser } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Utils from "../utils/Utils";
+import BackendService from "../services/BackendService";
 
 class NavigationBarClass extends React.Component {
   constructor(props) {
     super(props);
     this.goHome = this.goHome.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+  logout() {
+    BackendService.logout().then(() => {
+      Utils.removeUser();
+      this.goHome();
+    });
   }
 
   goHome() {
@@ -15,6 +25,7 @@ class NavigationBarClass extends React.Component {
   }
 
   render() {
+    let uname = Utils.getUserName();
     return (
       <Navbar bg="light" expand="lg">
         <Navbar.Brand>
@@ -23,7 +34,9 @@ class NavigationBarClass extends React.Component {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link as={Link} to="/home">
+              Home
+            </Nav.Link>
             <Nav.Link onClick={this.goHome}>Another Home</Nav.Link>
             <Nav.Link
               onClick={() => {
@@ -33,6 +46,17 @@ class NavigationBarClass extends React.Component {
               Yet Another Home
             </Nav.Link>
           </Nav>
+          <Navbar.Text>{uname}</Navbar.Text>
+          {uname && (
+            <Nav.Link onClick={this.logout}>
+              <FontAwesomeIcon icon={faUser} fixedWidth /> Выход
+            </Nav.Link>
+          )}
+          {!uname && (
+            <Nav.Link as={Link} to="/login">
+              <FontAwesomeIcon icon={faUser} fixedWidth /> Вход
+            </Nav.Link>
+          )}
         </Navbar.Collapse>
       </Navbar>
     );
